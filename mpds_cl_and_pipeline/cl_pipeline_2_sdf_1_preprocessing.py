6d870cab637c62da52386ecc3b55027ada1cb633
Hillul Chutia <40518024+Hillul-Sutia@users.noreply.github.com>
2026-05-04 15:23:35 +0530

    Refactor sdf_preprocessing_phase function
    Refactor sdf_preprocessing_phase to improve error handling and file checks.

--- mpds_cl_and_pipeline/cl_pipeline_2_sdf_1_preprocessing.py
+++ mpds_cl_and_pipeline/cl_pipeline_2_sdf_1_preprocessing.py
@@ -2,19 +2,16 @@ import os
 
 from .sdf_file_processing import preprocess_sdf
 
-def sdf_preprocessing_phase( current_database_dir ) :
+def sdf_preprocessing_phase(current_database_dir):
     
-    current_database_dir_files = os.listdir( current_database_dir )
-    
-    for file in current_database_dir_files:
-        
-        input_file_path = os.path.join( current_database_dir, file )
-        
-        if file.endswith('.sdf'):
-            preprocess_sdf( input_file_path )
-            
-        elif os.path.isdir( input_file_path ):
-            sdf_preprocessing_phase( input_file_path )
-            
-        else:
-            pass
+    for file in os.listdir(current_database_dir):
+        input_file_path = os.path.join(current_database_dir, file)
+
+        if os.path.isdir(input_file_path) and not os.path.islink(input_file_path):
+            sdf_preprocessing_phase(input_file_path)
+
+        elif file.lower().endswith('.sdf'):
+            try:
+                preprocess_sdf(input_file_path)
+            except Exception as e:
+                print(f"Error processing {input_file_path}: {e}")
